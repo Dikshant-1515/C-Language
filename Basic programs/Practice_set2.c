9e5b562517aba704aadac7ac5fc4192c7c0ad185
Dikshant-1515 <dikshant.jaryal08@gmail.com>
2023-02-08 14:50:22 +0530

    basic programs

--- Basic programs/Practice_set2.c
+++ Basic programs/Practice_set2.c
@@ -1,41 +1,41 @@
-//2.Q. 
-// #include<stdio.h>
-// int main(){
-//     float a = 3.0;
-//     int b = 8;
-//     int c = 2;
-//     printf("The value is %f",a/b - c);
-//     return 0;
-// }
+// 1.Q. 
+#include<stdio.h>
+int main(){
+    float a = 3.0;
+    int b = 8;
+    int c = 2;
+    printf("The value is %f",a/b - c);
+    return 0;
+}
 
 
 
-//3.Q. Number is divisible by 97 or not 
-//#include<stdio.h>
-// int main(){
-//     int a ;
-//     printf("Enter the number \n");
-//     scanf("%d",&a);
+// 2.Q. Number is divisible by 97 or not 
+#include<stdio.h>
+int main(){
+    int a ;
+    printf("Enter the number \n");
+    scanf("%d",&a);
 
-//     printf("Divisibility test returns: %d\n",a%97);
+    printf("Divisibility test returns: %d\n",a%97);
 
-//     return 0;
+    return 0;
 
-// }
+}
 
-//4.Q.
-// #include<stdio.h>
-// int main(){
-//     int x = 2;
-//     int y = 3;
-//     int z = 3;
-//     int k = 1;
-//     printf("The value is %d\n",3*x /y - z+k+34);
-//     return 0;
+// 3.Q.
+#include<stdio.h>
+int main(){
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+    printf("The value is %d\n",3*x /y - z+k+34);
+    return 0;
 
-//     // 6/3 - 3 + 1 + 34
-//     // 2 - 3 + 1 + 34
-//     // 0 + 34
+    // 6/3 - 3 + 1 + 34
+    // 2 - 3 + 1 + 34
+    // 0 + 34
 
-// }
+}
 
